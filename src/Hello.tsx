c55e0ad56908daf49f4044e28df62fa8fabd11e3
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-02-16 20:40:38 +0100

    test video

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,6 +1,7 @@
-import {Audio, Video} from 'remotion';
+import {Audio, delayRender, Video} from 'remotion';
 
 export const Hello: React.FC = (props) => {
+  delayRender()
   return (<>
     <Video src={props.vid} />
     <Audio src={props.sound} />
--- src/Video.tsx
+++ src/Video.tsx
@@ -1,17 +1,42 @@
-import {Composition} from 'remotion';
+import {Composition, continueRender, delayRender} from 'remotion';
 import {Hello} from './Hello';
 import vid from './1.webm'
 import sound from './1.mp3'
-//console.log(vid)
+import { useEffect, useState } from 'react';
 
 export const RemotionVideo: React.FC = () => {
+
+	const [handle] = useState(() => delayRender());
+	const [vid1duration, setVid1Duration] = useState([]);
+
+	const framesPerSecond: number = 60
+
+	let numberOfFrames: number = 1
+	useEffect(() => {
+			
+		const videoElement = document.createElement('video')
+		videoElement.setAttribute('src', vid)
+		videoElement.style.display = 'none'
+		console.log(document.querySelector('body'))
+		document.querySelector('body')?.appendChild(videoElement)
+		videoElement.addEventListener('loadeddata', () => {
+			console.log(parseInt(videoElement.duration * framesPerSecond))
+			setVid1Duration(parseInt(videoElement.duration * framesPerSecond))
+	 }, false);
+		continueRender(handle);
+	}, [handle]);
+
+	if (Number.isInteger(vid1duration)) {
+		numberOfFrames = vid1duration
+	}
+
 	return (
 		<>
 			<Composition
 				id="Hello"
 				component={Hello}
-				durationInFrames={150}
-				fps={30}
+				durationInFrames={numberOfFrames}
+				fps={framesPerSecond}
 				width={1920}
 				height={1080}
 				defaultProps={{
