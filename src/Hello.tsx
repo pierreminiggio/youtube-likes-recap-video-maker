1e44ef7c902aa06843a9fbaeb186a308ae76413a
Jonny Burger <jonathanburger11@gmail.com>
2021-02-17 23:41:58 +0100

    preserve formatting

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,23 +1,21 @@
 import {Audio, Sequence, Video} from 'remotion';
 
 export const Hello: React.FC = props => {
-  const {vids} = props
+  const vids = props.vids
 
   let start = 0
 
 	return (
 		<>
-			{vids.map(vid => {
-        const sequence = (
-	<Sequence
-		key={start}
-		from={start}
-		durationInFrames={vid.duration}
-	>
-		<Video src={vid.video} />
-		<Audio src={vid.audio} />
-	</Sequence>
-)
+      {vids.map(vid => {
+        const sequence = <Sequence
+          key={start}
+          from={start}
+          durationInFrames={vid.duration}
+        >
+          <Video src={vid.video} />
+          <Audio src={vid.audio} />
+        </Sequence>
 
         start += vid.duration + 1
         
--- src/Video.tsx
+++ src/Video.tsx
@@ -1,101 +1,97 @@
 import {useEffect, useState} from 'react';
 import {Composition, continueRender, delayRender} from 'remotion';
+import sound from './1.mp3';
+import vid from './1.webm';
 import {Hello} from './Hello';
-import VideoToLoad from './VideoToLoad';
+import VideoToLoad from './VideoToLoad'
 
-const baseStorageUrl = 'https://storage.miniggiodev.fr/youtube-likes-recap';
-const vidsToLoad: VideoToLoad[] = [];
+const baseStorageUrl = 'https://storage.miniggiodev.fr/youtube-likes-recap'
+const vidsToLoad: VideoToLoad[] = []
 
-const intros: number[] = [1, 2, 3, 4, 5, 6, 7];
-const twoPartsIntro: number[] = [5];
+const intros: number[] = [1, 2, 3, 4, 5, 6, 7]
+const twoPartsIntro: number[] = [5]
 
-const introVideoStorage: string = baseStorageUrl + '/intro/';
-const pickedIntro: number = intros[Math.floor(Math.random() * intros.length)];
-const isTwoPartIntro: boolean = twoPartsIntro.includes(pickedIntro);
+const introVideoStorage: string = baseStorageUrl + '/intro/'
+const pickedIntro: number = intros[Math.floor(Math.random() * intros.length)]
+const isTwoPartIntro: boolean = twoPartsIntro.includes(pickedIntro)
 
-const introVideoUrl: string = isTwoPartIntro
-	? introVideoStorage + '/' + pickedIntro + '-1'
-	: introVideoStorage + '/' + pickedIntro;
+const introVideoUrl: string = isTwoPartIntro ?
+	(introVideoStorage + '/' + pickedIntro + '-1') :
+	(introVideoStorage + '/' + pickedIntro)
 
-vidsToLoad.push(VideoToLoad.makeFromURL(introVideoUrl));
+vidsToLoad.push(VideoToLoad.makeFromURL(introVideoUrl))
 
-const today: Date = new Date();
-const day: number = today.getDate();
-const dayVideoUrl: string = baseStorageUrl + '/number/' + day;
-vidsToLoad.push(VideoToLoad.makeFromURL(dayVideoUrl));
+const today: Date = new Date()
+const day: number = today.getDate()
+const dayVideoUrl: string = baseStorageUrl + '/number/' + day
+vidsToLoad.push(VideoToLoad.makeFromURL(dayVideoUrl))
 
-const month: number = today.getMonth() + 1;
-const monthVideoUrl: string = baseStorageUrl + '/month/' + month;
-vidsToLoad.push(VideoToLoad.makeFromURL(monthVideoUrl));
+const month: number = today.getMonth() + 1
+const monthVideoUrl: string = baseStorageUrl + '/month/' + month
+vidsToLoad.push(VideoToLoad.makeFromURL(monthVideoUrl))
 
 const multipleTakesYear: {[key: number]: number} = {
-	2021: 2,
-};
+	2021: 2
+}
 
-const yearVideoStorage: string = baseStorageUrl + '/year/';
-const year: number = today.getFullYear();
-const yearTakes: number | undefined = multipleTakesYear[year];
+const yearVideoStorage: string = baseStorageUrl + '/year/'
+const year: number = today.getFullYear()
+const yearTakes: number|undefined = multipleTakesYear[year]
 
-const yearVideoUrl: string = yearTakes
-	? yearVideoStorage +
-	  '/' +
-	  year +
-	  '-' +
-	  (Math.floor(Math.random() * yearTakes) + 1)
-	: yearVideoStorage + '/' + year;
+const yearVideoUrl: string = yearTakes ?
+	(yearVideoStorage + '/' + year + '-' + (Math.floor(Math.random() * yearTakes) + 1)) :
+	(yearVideoStorage + '/' + year)
 
-vidsToLoad.push(VideoToLoad.makeFromURL(yearVideoUrl));
+vidsToLoad.push(VideoToLoad.makeFromURL(yearVideoUrl))
 
 const videoTakes: {[key: number]: number} = {
 	1: 1,
 	2: 1,
 	3: 1,
-	4: 1,
-};
+	4: 1
+}
 
 export const RemotionVideo: React.FC<{
-	propOne: string;
-	propTwo: number;
+  propOne: string;
+  propTwo: number;
 }> = (propOne, propTwo) => {
-	console.log(propOne);
-	console.log(propTwo);
+	console.log(propOne)
+	console.log(propTwo)
 	const [handle] = useState(() => delayRender());
 	const [vidDuration, setVidDuration] = useState(0);
 
 	const framesPerSecond = 59.94;
 
 	useEffect(() => {
-		const promises: Promise<number>[] = [];
 
-		vidsToLoad.forEach((vidToLoad) => {
-			promises.push(
-				new Promise((resolve) => {
-					const videoElement = document.createElement('video');
-					videoElement.setAttribute('src', vidToLoad.video);
-					videoElement.style.display = 'none';
-					document.querySelector('body')?.appendChild(videoElement);
-					videoElement.addEventListener(
-						'loadeddata',
-						() => {
-							const duration: number = Math.floor(
-								videoElement.duration * framesPerSecond
-							);
-							vidToLoad.duration = duration;
-							resolve(duration);
-						},
-						false
-					);
-				})
-			);
-		});
+		const promises: Promise<number>[] = []
 
-		Promise.all(promises).then((durations) => {
-			let totalDuration = 0;
-			durations.forEach((duration) => {
-				totalDuration += duration;
-			});
-			setVidDuration(totalDuration);
-		});
+		vidsToLoad.forEach(vidToLoad => {
+			promises.push(new Promise(resolve => {
+				const videoElement = document.createElement('video');
+				videoElement.setAttribute('src', vidToLoad.video);
+				videoElement.style.display = 'none';
+				document.querySelector('body')?.appendChild(videoElement);
+				videoElement.addEventListener(
+					'loadeddata',
+					() => {
+						const duration: number = Math.floor(videoElement.duration * framesPerSecond)
+						vidToLoad.duration = duration
+						resolve(duration);
+					},
+					false
+				);
+			}))
+		})
+		
+		Promise.all(promises).then(durations => {
+			let totalDuration = 0
+			durations.forEach(duration => {
+				totalDuration += duration
+			})
+			setVidDuration(totalDuration)
+		})
+
 	}, [handle]);
 
 	useEffect(() => {
@@ -118,7 +114,7 @@ export const RemotionVideo: React.FC<{
 				width={1920}
 				height={1080}
 				defaultProps={{
-					vids: vidsToLoad,
+					vids: vidsToLoad
 				}}
 			/>
 		</>
